75721dd64cf563cbaa798c662fa5930e7e6436d6
Khang Nguyen <ngkhang0220@gmail.com>
2026-01-27 22:17:38 +0700

    feat(app): integrates morgan, logging, and compression middlewares
    - `morgan` for HTTP request logging
    - `helmet` to secure HTTP response headers
    - `cookie-parser` for parsing cookie headers
    - `compression` to enable gzip response compression

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,28 @@
  * Last Updated: 2026-01-27
  ------------------------------------------------- */
 
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
 import type { Express, Response, Request } from 'express';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 import { StatusCodes, ReasonPhrases } from 'http-status-codes';
-
 
 export const createApp = (): Express => {
   const app = express();
 
-  // Initial middleware
+  // HTTP request logger middleware
+  app.use(morgan('dev'));
+  // Secure Express apps by setting HTTP response headers
+  app.use(helmet());
+  // Parse Cookie header and populate req.cookies
+  app.use(cookieParser());
+  // Enable gzip compression
+  app.use(compression());
+  // Middleware to parse URL-encoded request bodies
   app.use(express.urlencoded({ extended: true }));
+  // Middleware to parse JSON request bodies
   app.use(express.json());
 
   // Routes
